03f469775ce547b0533877fb76e82ff225cc2707
Oliver's Tech Corner <gmail@oliverstech.tk>
2022-08-28 20:27:15 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
   author = 'oliverstech',                   # Type in your name
   author_email = 'mail@oliverstech.tk',      # Type in your E-Mail
   url = 'https://github.com/oliverstech/pykuma',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/oliverstech/pykuma/archive/refs/tags/1.0.tar.gz',    # I explain this later on
   keywords = ['uptime', 'api', 'pykuma', 'kuma'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'requests',
+          'asyncio',
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
